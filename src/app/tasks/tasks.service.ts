fded1348752668f7ae84961e24c257255f48a234
mdfarooqs1111@gmail.com <farooq.m@outplayhq.com>
2024-09-04 17:43:49 +0530

    local storage added

--- src/app/tasks/tasks.service.ts
+++ src/app/tasks/tasks.service.ts
@@ -6,6 +6,14 @@ import { type NewTaskData } from './task/task.model';
 export class TasksService {
  private tasks = dummyTasks;
 
+ constructor(){
+    const tasks = localStorage.getItem('tasks');
+
+    if(tasks){
+        this.tasks = JSON.parse(tasks);
+    }
+ }
+
   getUserTasks(userId: string) {
     return this.tasks.filter((task) => task.userId === userId);
   }
@@ -18,10 +26,16 @@ export class TasksService {
         summary : taskData.summary,
         dueDate : taskData.date,
       })
+      this.saveTasks();
   }
 
 
   removeTask(id:string){
     this.tasks = this.tasks.filter((task) => task.id != id);
+    this.saveTasks();
+  }
+
+  private saveTasks(){
+    localStorage.setItem('tasks',JSON.stringify(this.tasks));
   }
 }
